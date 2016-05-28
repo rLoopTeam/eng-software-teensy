2259e56225cd00180cb55c70c5374c2d3b425567
Piense <piense@gmail.com>
2016-05-28 12:15:17 -0700

    Update readme and rangefinder template

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 eng-software team repo for Teensy code
 
 Set your arduino sketchbook directory to the one you cloned from the repo and you should be good to go.
+
+Make sure none of the libraries in the sketchbook are also in your global libraries directory (C:\Program Files (x86)\Arduino\hardware\teensy\avr\libraries) as they will supersede the modified versions we're using.
--- Sketchbook/Rangefinder/Rangefinder.ino
+++ Sketchbook/Rangefinder/Rangefinder.ino
@@ -13,37 +13,39 @@ int BNO_Error = 0;
 
 IntervalTimer controlTimer;
 
-uint32_t lastMicros;
-
-
-//Blinks the LED on the Teensy
-//as a heartbeat from the control loop
 float blinkRate;
 int blinkCount;
 
-//Measures the timing of
-//the control loop
+//Define's ADC range based off of ADC resolution
+int ADCmaxValue;
+
+//Measure the control loop timing utilization
 uint32_t duration;
 uint32_t sensors;
 uint32_t readingPi;
 uint32_t processingPi;
 uint32_t transmitting;
+uint32_t lastMicros;
+
+int bnoCalibration;
+
+
 
 void recvParam(rI2CRX_decParam decParam);
 
 void setup(void)
 {
 
+
   pinMode(15,OUTPUT);
   digitalWrite(15,HIGH);
-  
+
+  //Iniitialize the I2C bus for the BNO sensor
   Wire1.begin(I2C_MASTER, 0, I2C_PINS_29_30, I2C_PULLUP_INT, I2C_RATE_100);
 
-  //Resets the BNO if it got interrupted
-  //in an I2C transaction
+  int tries = 0;
+
   do{
-    Wire1.resetBus();
-  
     BNO_Error = 0;
   
     // Initialise the sensor
@@ -54,30 +56,51 @@ void setup(void)
     }
   
     if(BNO_Error == 1){
-  
       digitalWrite(15,LOW);
-      delay(1000);
+      delay(50); //Data sheet says at least 20ns
       digitalWrite(15,HIGH);
-      delay(1000);
+      delay(750); //Data sheet says 650 ms
     }
-  }while(BNO_Error == 1);
+
+    tries++;
+  }while(BNO_Error == 1 && tries <= 3);
     
   //Let the BNO get settled
   delay(2000);
   bno.setExtCrystalUse(true);
 
+  //LED is using PWM from some old code, could be changed to a simple digital output
+  pinMode(13,OUTPUT); //LED Pin
+  analogWriteFrequency(5,490); //PWM output at 350 Hz
+  analogWriteResolution(16); //PWM output at 16 bit resolution
+  ADCmaxValue = 65535;
+
   //Open the I2C interface to the Pi
   Wire.begin(I2C_MASTER, 0, I2C_PINS_18_19, I2C_PULLUP_INT, I2C_RATE_1000, I2C_OP_MODE_DMA);
+  Wire.resetBus();
 
+  //Initialize the Pi to Teensy I2C data procotol and connect the callbacks
   rI2CRX_begin();
-
   rI2CRX_recvDecParamCB = &recvParam;
   rI2CRX_frameRXBeginCB = &gotAFrame;
   rI2CRX_frameRXEndCB = &endFrame;
 
+  //See if the BNO is calibrated
+  if(bno.readSensorOffsetsFromEEPROM(0))
+    bnoCalibration = 1;
+  else
+    bnoCalibration = 0;
+
+  //***************************
+  //Initialize the rangefinder here
+  //Probably send <esc> then commands to put it in continous sample mode
+  //Could store the samples in a buffer via DMA or just let the control loop block
+  //while it waits for a complete sample.
+  //***************************
 
   blinkRate = 50;
   blinkCount = 0;
+
 
   //The I2C Library needs to be able to fire some interrupts
   //during our control loop to manage the start & stop
@@ -86,39 +109,44 @@ void setup(void)
 
     //Setup the 100Hz control loop timer
   controlTimer.begin(ControlLoop, 10000);
-
-  angleSetPoint = 0;
 }
 
 void loop(void)
 {
-
+  //Don't put anything here
 }
 
+//A valid frame from the Pi has arrived
 void gotAFrame()
 {}
 
+//The frame from the Pi has reached the end
 void endFrame()
-{
-  updateOuputs();
-}
+{}
 
 //Called whenever a new
 //parameter value is received from the
 //Pi via the I2C bus
 void recvParam(rI2CRX_decParam decParam)
 {
-  //Example Code
-  /*
+
+/* Simpel example parameter
   if(decParam.index == 1 && decParam.type == rI2C_FLOAT)
     engineSV[0] = *((float*)decParam.val);
-  if(decParam.index == 2 && decParam.type == rI2C_FLOAT)
-    engineSV[1] = *((float*)decParam.val);
-  if(decParam.index == 3 && decParam.type == rI2C_FLOAT)
-    engineSV[2] = *((float*)decParam.val);
-  if(decParam.index == 4 && decParam.type == rI2C_FLOAT)
-    engineSV[3] = *((float*)decParam.val);*/
+*/
 
+  //Write BNO calibration to flash
+  if(decParam.index == 19 && decParam.type == rI2C_FLOAT){
+    if(bno.writeSensorOffsetsToEEPROM(0))
+      bnoCalibration = 16;
+    else
+      bnoCalibration = 0;
+    uint8_t system1, gyro, accel, mag;
+    bno.getCalibration(&system1, &gyro, &accel, &mag);
+    bnoCalibration += system1/3 + gyro/3*2 + accel/3 * 4 + mag/3 * 8;
+  }
+
+  //A simple test parameter that changes how fast the LED on the Teensy blinks
   if(decParam.index == 100 && decParam.type == rI2C_FLOAT)
     blinkRate = *((float*)decParam.val);
 }
@@ -131,53 +159,48 @@ void ControlLoop(void)
   
   uint32_t beginM = micros();
 
-  rI2CTX_beginFrame();/*
+  rI2CTX_beginFrame();
+
+  //Timing stats on the control loop
+  /*
   rI2CTX_addParameter(26,(int32_t)sensors);
   rI2CTX_addParameter(27,(int32_t)readingPi);
   rI2CTX_addParameter(28,(int32_t)processingPi);
   rI2CTX_addParameter(29,(int32_t)transmitting);
   rI2CTX_addParameter(30,(int32_t)duration);*/
 
-  // Get a new sensor event 
+  //Get the latest data from the BNO 9DOF
   sensors_event_t event;
-
   bno.getEvent(&event);
-  
-  rI2CTX_addParameter(30,event.orientation.x);  //This one
- // rI2CTX_addParameter(31,event.orientation.y);
-  //rI2CTX_addParameter(32,event.orientation.z);
 
-//  mma.read();
-//  mma.getEvent(&event);
-//  rI2CTX_addParameter(33,event.acceleration.x);
-//  rI2CTX_addParameter(34,event.acceleration.y);
-//  rI2CTX_addParameter(35,event.acceleration.z);
+  //Add BNO data to be sent to the Pi
+  rI2CTX_addParameter(30,event.orientation.x);
+  rI2CTX_addParameter(31,event.orientation.y);
+  rI2CTX_addParameter(32,event.orientation.z);
+  rI2CTX_addParameter(49,(int32_t)bnoCalibration);
 
-  rI2CTX_addParameter(75,(float)error);
-  rI2CTX_addParameter(76,(float)errorI);
 
-  rI2CTX_addParameter(10,(float)servoSV[0]);
-  rI2CTX_addParameter(11,(float)servoSV[1]);
-  rI2CTX_addParameter(12,(float)servoSV[2]);
-  rI2CTX_addParameter(13,(float)servoSV[3]);
+  float rangefinderDist = 0;
+  //***************************
+  //Process data from the rangefinder here
+  //***************************
+  rI2CTX_addParameter(10, rangefinderDist);
   
   rI2CTX_addParameter(50,(int32_t)i);
   
   rI2CTX_endFrame();
 
   sensors = micros() - beginM;
-
-
   readingPi = micros();
 
-  int request = 100;
+  //Request control data fromt the pi
   Wire.requestFrom(51, 100, 500);
   uint8_t recvByte;
 
   readingPi = micros() - readingPi;
-
   processingPi = micros();
-  
+
+  //Process any data sent over from the Pi
   while (Wire.available())
   {
     recvByte = Wire.readByte();
@@ -185,16 +208,20 @@ void ControlLoop(void)
   }
 
   processingPi = micros() - processingPi;
-  
+
+  //Send the log data to the Pi over I2C
   Wire.beginTransmission(51);
   Wire.write(rI2CTX_buffer, rI2CTX_bufferPos);
-
-    transmitting = micros();
+  
+  transmitting = micros();
+  //still a blocking call atm, will eventually done with nonblocking DMA transaction
   Wire.endTransmission(I2C_STOP, 3000);
 
   transmitting = micros() - transmitting;
   
   i++;
+
+  //Blink the LED so we know the control loop is still alive
   blinkCount++;
   if(blinkCount == blinkRate)
     analogWrite(13, ADCmaxValue);
